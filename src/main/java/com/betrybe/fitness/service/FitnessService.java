dec67e5723fa3944160a8b4381bf6cb942267e9d
eon <bernardomp.dev@gmail.com>
2023-08-31 17:18:16 -0300

    [FitnessService]Feat: getWorkout.

--- src/main/java/com/betrybe/fitness/service/FitnessService.java
+++ src/main/java/com/betrybe/fitness/service/FitnessService.java
@@ -3,6 +3,7 @@ package com.betrybe.fitness.service;
 import com.betrybe.fitness.database.FakeFitnessDatabase;
 import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
+import com.betrybe.fitness.model.Workout;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class FitnessService implements FitnessServiceInterface {
 
+  private final FakeFitnessDatabase repository;
+
   @Autowired
-  public FitnessService(FakeFitnessDatabase repository) {}
+  public FitnessService(FakeFitnessDatabase repository) {
+    this.repository = repository;
+  }
 
   @Override
   public WorkoutDto saveWorkout(WorkoutCreationDto newWorkoutDto) {
@@ -24,7 +29,14 @@ public class FitnessService implements FitnessServiceInterface {
 
   @Override
   public Optional<WorkoutDto> getWorkout(Long id) {
-    return Optional.empty();
+    Optional<Workout> workout = repository.getWorkout(id);
+
+    return workout.map(value -> new WorkoutDto(
+        value.getId(),
+        value.getName(),
+        value.getRepetitions()
+    ));
+
   }
 
   @Override

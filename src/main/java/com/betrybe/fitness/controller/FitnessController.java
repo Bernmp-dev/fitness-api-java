3e6bcb4faa00c2bb381afef459860f0bd95a9a8a
eon <bernardomp.dev@gmail.com>
2023-09-01 14:34:39 -0300

    [FitnessController]Feat: GET /fitness/workouts.

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -2,6 +2,7 @@ package com.betrybe.fitness.controller;
 
 import com.betrybe.fitness.dto.*;
 import com.betrybe.fitness.service.FitnessServiceInterface;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,17 @@ public class FitnessController implements FitnessControllerInterface {
   @GetMapping("/fitness")
   public String getFitness() {
     return "Boas vindas à API de Fitness!";
+  }
+
+  @GetMapping("/fitness/workouts")
+  public ResponseEntity<List<WorkoutDto>> getAllWorkouts() {
+    List<WorkoutDto> result = fitnessService.getAllWorkouts();
+
+    if (result != null) {
+      return ResponseEntity.ok(result);
+    }
+
+    return ResponseEntity.notFound().build();
   }
 
   /**
